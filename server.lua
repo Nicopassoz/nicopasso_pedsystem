e9ba17c287d193dcc4c9824df9899680ebb90278
Nico Passo <83984711+Nicopassoz@users.noreply.github.com>
2022-06-28 13:53:59 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,94 +1,72 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-AddEventHandler("esx:playerLoaded", function(source) 
-	local set = false
-	Citizen.Wait(8000)--ITA: questa sarebbe la funzione che quando un player è spawnato aspetta i secondi nel wait e 
-	local xPlayer = ESX.GetPlayerFromId(source) --poi checca nel db se uno ha un ped e se ce l'ha glielo setta
-		if not set then	 --ENG: this would be the function that when a player is spawned it waits for the seconds in the citizem.wait and
-			MySQL.Async.fetchAll("SELECT identifier FROM ped WHERE identifier = @steam", {	--then queer in the db if one has a ped and if he does, he sets it to him
-		["@steam"] = xPlayer.identifier
-	}, function (result)
-		if #result ~= 0 then
-			local peddamettere = MySQL.Sync.fetchScalar("SELECT ped FROM ped WHERE identifier = @steam", {
-				["@steam"] = xPlayer.identifier})
-			TriggerClientEvent('nicopasso_settoilfottutoped', source, peddamettere)
-		end
+AddEventHandler("esx:playerLoaded", function(source)
+	Citizen.Wait(6000)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    MySQL.Async.fetchAll("SELECT ped FROM ped WHERE identifier = @identifier", {
+        ["@identifier"] = xPlayer.identifier
+    }, function(result)
+        if result[1] then
+            TriggerClientEvent('nicopasso:setped', src, result[1].ped)
+        end
+    end)
 end)
-  		set = true
+
+RegisterCommand('setped', function(source, args)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local id = args[1]
+	local ped = args[2]
+	if xPlayer.getGroup() == 'admin' then
+		if id ~= nil and ped ~= nil then
+			local xPlayerPed = ESX.GetPlayerFromId(id)
+			MySQL.Async.fetchAll("SELECT ped FROM ped WHERE identifier = @identifier", {
+				["@identifier"] = xPlayerPed.identifier
+			}, function (result)
+				if not result[1] or ped ~= result[1].ped then
+					TriggerClientEvent('nicopasso:setped', id, ped)
+					TriggerClientEvent('esx:showNotification', source, 'Ped settato al player con successo')
+					if result[1] then
+						MySQL.Async.execute("UPDATE ped SET ped = @ped WHERE identifier = @identifier",
+						{['@identifier'] = xPlayerPed.identifier, ['@ped'] = ped})
+					else
+						MySQL.Async.execute('INSERT INTO ped (identifier, ped) VALUES (@identifier, @ped)',
+						{['@identifier'] = xPlayerPed.identifier, ['@ped'] = ped})
+					end	
+				else
+					TriggerClientEvent('esx:showNotification', source, 'Stai settando il ped che il player già ha!')
+				end
+			end)
+		end
 	end
 end)
 
-RegisterCommand('setped', function(source, args) -- ITA:comando per il set del ped --ENG: command for set a ped to players
-		local xPlayer = ESX.GetPlayerFromId(source) 
-		id = args[1]
-		ped = args[2]
-		local xPlayerPed = ESX.GetPlayerFromId(id)
-		local trovato = false
-		if xPlayer.getGroup() == 'superadmin' or xPlayer.getGroup() == 'admin' then -- ITA: check dei permessi del player che fa il comando ENG: check of the permission for the person that is doing the command
-			if id ~= nil and ped ~= nil then
-				if GetPlayerName(tonumber(args[1])) ~= nil then
-					for i=1, #Config.Ped.Ped, 1 do
-						local peddino = Config.Ped.Ped[i]
-						if ped == peddino.comando then
-							MySQL.Async.fetchAll("SELECT ped FROM ped WHERE identifier = @steam", {
-								["@steam"] = xPlayerPed.identifier
-							}, function (result) -- ITA: qui checcka se un player ha gia un ped salvato nel db e se lo ha gia aggiorna la colonna del ped nel db, sennò la crea 
-								if #result ~= 0 then --ENG: here it check if a player already has a ped saved in the db and if he already has it updates the column of the ped in the db, otherwise he creates it
-									if result[1].ped ~= peddino.nome then -- ITA: qui checca invece se il ped che setti è uguale a quello che il player ha gia
-										TriggerClientEvent('nicopasso_settoilfottutoped', id, peddino.nome)-- ENG: here it check if the ped that you are setting is equal to the one the player already has
-										MySQL.Async.execute("UPDATE ped SET ped = @ped WHERE identifier = @identifier",
-										{['@identifier'] = xPlayerPed.identifier, ['@ped'] = peddino.nome}) 
-									else
-										TriggerClientEvent('esx:showNotification', source, 'Stai settando il ped che il player già ha!')
-									end
-								else
-									TriggerClientEvent('nicopasso_settoilfottutoped', id, peddino.nome)
-									MySQL.Async.execute('INSERT INTO ped (identifier, ped) VALUES (@identifier, @ped)',
-									{['@identifier'] = xPlayerPed.identifier, ['@ped'] = peddino.nome})
-								end
-									end)
-							trovato = true
-						end
-					end
-					if not trovato then
-						TriggerClientEvent('esx:showNotification', source, 'Ped non valido')
-					end
-				else
-                	TriggerClientEvent('esx:showNotification', source, 'Player Non online!')
-            	end
-			else
-				TriggerClientEvent('esx:showNotification', source, "Devi specficare l'id e il ped da settare!")
-			end
-		else 
-			TriggerClientEvent('esx:showNotification', source, 'Non hai i permessi')
-		end
-end)
-
-RegisterCommand('resetped', function(source, args) -- ITA: COMANDO PER IL RESET DEL PED --ENG: COMMAND FOR RESET PED TO A PLAYER
-	local xPlayer = ESX.GetPlayerFromId(source)
+RegisterCommand('resetped', function(source, args) -- ITA: COMANDO PER IL RESET DEL PED   --ENG: COMMAND FOR RESET PED TO A PLAYER
+    local src = source
+	local xPlayer = ESX.GetPlayerFromId(src)
 	id = args[1]
 	local xPlayerPed = ESX.GetPlayerFromId(id)
-	if xPlayer.getGroup() == 'superadmin' or xPlayer.getGroup() == 'admin' then -- ITA: check dei permessi del player che fa il comando --ENG: check of the permission for the person that is doing the command
+	if xPlayer.getGroup() == 'admin' then
 		if id ~= nil then
 			if GetPlayerName(tonumber(args[1])) ~= nil then
-				MySQL.Async.fetchAll("SELECT identifier FROM ped WHERE identifier = @steam", {
-				["@steam"] = xPlayerPed.identifier
-				}, function (result) 
-					if #result ~= 0 then
+				MySQL.Async.fetchAll("SELECT identifier FROM ped WHERE identifier = @identifier", {
+				["@identifier"] = xPlayerPed.identifier
+				}, function (result)
+					if result[1] then
 						MySQL.Async.execute("DELETE FROM ped WHERE identifier=@identifier", { ["@identifier"] = result[1].identifier })
-						TriggerClientEvent('reset', id)
+						TriggerClientEvent('nicopasso:resetped', id)
 					else 
-						TriggerClientEvent('esx:showNotification', source, 'Errore, sembra che il player non abbia un ped settato')
+						TriggerClientEvent('esx:showNotification', src, 'Errore, sembra che il player non abbia un ped settato')
 					end
 				end)
 			else
-				TriggerClientEvent('esx:showNotification', source, 'Player Non online!')
+				TriggerClientEvent('esx:showNotification', src, 'Player Non online!')
 			end
 		else
-			TriggerClientEvent('esx:showNotification', source, "Devi specficare l'id del player!")
+			TriggerClientEvent('esx:showNotification', src, "Devi specficare l'id del player!")
 		end
 	else 
-		TriggerClientEvent('esx:showNotification', source,'Non hai i permessi')
+		TriggerClientEvent('esx:showNotification', src,'Non hai i permessi')
 	end
 end)
